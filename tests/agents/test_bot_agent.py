98d1f64ca56170fe4ba5ee5d6125a6e21e1fdab5
Mike Storey <mike@agile-learning.institute>
2025-03-19 15:02:58 -0400

    updated testing to support logging changes

--- tests/agents/test_bot_agent.py
+++ tests/agents/test_bot_agent.py
@@ -13,9 +13,10 @@ class TestBotAgent(unittest.TestCase):
         self.add_channel = bot_agent.actions["add_channel"]["function"]
         self.remove_channel = bot_agent.actions["remove_channel"]["function"]
 
+    @patch("stage0_py_utils.agents.bot_agent.create_echo_breadcrumb")  
     @patch("stage0_py_utils.agents.bot_agent.create_echo_token")  
     @patch("stage0_py_utils.services.bot_services.BotServices.get_bot")
-    def test_get_bot_success(self, mock_get_bot, mock_create_token):
+    def test_get_bot_success(self, mock_get_bot, mock_create_token, mock_create_breadcrumb):
         """Test successful execution of get_bot action."""
         mock_bot_object = {
             "_id": "12345",
@@ -28,6 +29,7 @@ class TestBotAgent(unittest.TestCase):
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = {"atTime":"sometime", "correlationId":"correlation_ID"}
         mock_get_bot.return_value = mock_bot_object
         
         # Call function
@@ -36,16 +38,19 @@ class TestBotAgent(unittest.TestCase):
         
         # Assertions
         mock_create_token.assert_called_once()
-        mock_get_bot.assert_called_once_with(bot_id=arguments, token='fake_token')
+        mock_create_breadcrumb.assert_called_once()
         self.assertEqual(result, mock_bot_object)
+        mock_get_bot.assert_called_once_with(bot_id=arguments, token='fake_token')
             
+    @patch("stage0_py_utils.agents.bot_agent.create_echo_breadcrumb")  
     @patch("stage0_py_utils.agents.bot_agent.create_echo_token")  
     @patch("stage0_py_utils.services.bot_services.BotServices.get_bot")
-    def test_get_bot_fail(self, mock_get_bot, mock_create_token):
+    def test_get_bot_fail(self, mock_get_bot, mock_create_token, mock_create_breadcrumb):
         """Test failure case for get_bot action."""
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = {"atTime":"sometime", "correlationId":"correlation_ID"}
         mock_get_bot.side_effect = Exception("Test Exception")
         
         # Call function
@@ -54,17 +59,20 @@ class TestBotAgent(unittest.TestCase):
         
         # Assertions
         mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once()
         mock_get_bot.assert_called_once_with(bot_id=arguments, token='fake_token')
         self.assertEqual(result, "error")
         
+    @patch("stage0_py_utils.agents.bot_agent.create_echo_breadcrumb")  
     @patch("stage0_py_utils.agents.bot_agent.create_echo_token")  
     @patch("stage0_py_utils.services.bot_services.BotServices.get_channels")
-    def test_get_channels_success(self, mock_get_channels, mock_create_token):
+    def test_get_channels_success(self, mock_get_channels, mock_create_token, mock_create_breadcrumb):
         """Test successful execution of get_bot action."""
         mock_channels = ["channel_1", "channel_2"]
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = {"atTime":"sometime", "correlationId":"correlation_ID"}
         mock_get_channels.return_value = mock_channels
         
         # Call function
@@ -73,16 +81,19 @@ class TestBotAgent(unittest.TestCase):
         
         # Assertions
         mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once()
         mock_get_channels.assert_called_once_with(bot_id=arguments, token='fake_token')
         self.assertEqual(result, mock_channels)
     
+    @patch("stage0_py_utils.agents.bot_agent.create_echo_breadcrumb")  
     @patch("stage0_py_utils.agents.bot_agent.create_echo_token")  
     @patch("stage0_py_utils.services.bot_services.BotServices.get_channels")
-    def test_get_channels_fail(self, mock_get_channels, mock_create_token):
+    def test_get_channels_fail(self, mock_get_channels, mock_create_token, mock_create_breadcrumb):
         """Test failure case for get_bot action."""
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = {"atTime":"sometime", "correlationId":"correlation_ID"}
         mock_get_channels.side_effect = Exception("Test Exception")
         
         # Call function
@@ -91,6 +102,7 @@ class TestBotAgent(unittest.TestCase):
         
         # Assertions
         mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once()
         mock_get_channels.assert_called_once_with(bot_id=arguments, token='fake_token')
         self.assertEqual(result, "error")
         
@@ -103,7 +115,8 @@ class TestBotAgent(unittest.TestCase):
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
-        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        fake_breadcrumb = {"atTime":"sometime", "correlationId":"correlation_ID"}
+        mock_create_breadcrumb.return_value = fake_breadcrumb
         mock_add_channel.return_value = mock_channels
         
         # Call function
@@ -116,7 +129,7 @@ class TestBotAgent(unittest.TestCase):
         # Assertions
         mock_create_token.assert_called_once()
         mock_create_breadcrumb.assert_called_once_with("fake_token")
-        mock_add_channel.assert_called_once_with(channel_id='test_channel', bot_id='test_bot_id', token='fake_token', breadcrumb='fake_breadcrumb')
+        mock_add_channel.assert_called_once_with(channel_id='test_channel', bot_id='test_bot_id', token='fake_token', breadcrumb=fake_breadcrumb)
         self.assertEqual(result, mock_channels)
     
     @patch("stage0_py_utils.agents.bot_agent.create_echo_token")  
@@ -127,7 +140,8 @@ class TestBotAgent(unittest.TestCase):
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
-        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        fake_breadcrumb = {"atTime":"sometime", "correlationId":"correlation_ID"}
+        mock_create_breadcrumb.return_value = fake_breadcrumb
         mock_add_channel.side_effect = Exception("Test Exception")
         
         # Call function
@@ -143,7 +157,7 @@ class TestBotAgent(unittest.TestCase):
         mock_add_channel.assert_called_once_with(
             channel_id='test_channel', 
             bot_id='test_bot_id', 
-            token='fake_token', breadcrumb='fake_breadcrumb')
+            token='fake_token', breadcrumb=fake_breadcrumb)
         self.assertEqual(result, "error")
         
     @patch("stage0_py_utils.agents.bot_agent.create_echo_token")  
@@ -155,7 +169,8 @@ class TestBotAgent(unittest.TestCase):
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
-        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        fake_breadcrumb = {"atTime":"sometime", "correlationId":"correlation_ID"}
+        mock_create_breadcrumb.return_value = fake_breadcrumb
         mock_remove_channel.return_value = mock_channels
         
         # Call function
@@ -171,7 +186,7 @@ class TestBotAgent(unittest.TestCase):
         mock_remove_channel.assert_called_once_with(
             bot_id='test_bot_id', 
             channel_id='test_channel', 
-            token='fake_token', breadcrumb='fake_breadcrumb')
+            token='fake_token', breadcrumb=fake_breadcrumb)
         self.assertEqual(result, mock_channels)
     
     @patch("stage0_py_utils.agents.bot_agent.create_echo_token")  
@@ -182,7 +197,8 @@ class TestBotAgent(unittest.TestCase):
         
         # Mock return values
         mock_create_token.return_value = "fake_token"
-        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        fake_breadcrumb = {"atTime":"sometime", "correlationId":"correlation_ID"}
+        mock_create_breadcrumb.return_value = fake_breadcrumb
         mock_remove_channel.side_effect = Exception("Test Exception")
         
         # Call function
@@ -198,7 +214,7 @@ class TestBotAgent(unittest.TestCase):
         mock_remove_channel.assert_called_once_with(
             bot_id='test_bot_id', 
             channel_id='test_channel', 
-            token='fake_token', breadcrumb='fake_breadcrumb')
+            token='fake_token', breadcrumb=fake_breadcrumb)
         self.assertEqual(result, "error")
         
 if __name__ == "__main__":
