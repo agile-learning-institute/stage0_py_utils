47b6ccbad8cd57ed0ea5af6b730e56196d7e2f23
Mike Storey <mike@agile-learning.institute>
2025-04-08 11:52:13 -0400

    bump patch level, update docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="stage0_py_utils",
-    version="0.1.4",
+    version="0.1.5",
     description="A utility package for stage0 microservices",
     author="Your Name",
     author_email="your.email@example.com",
